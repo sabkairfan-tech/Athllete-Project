910a8e6e3dae584c57a7eabb9cd96cabc1cbef71
irfan hanif <sabkairfan@gmail.com>
2026-06-08 20:59:42 +0300

    done

--- Model/userModel.js
+++ Model/userModel.js
@@ -18,6 +18,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  isActive:{
+    type: Boolean,
+    default: true 
+  }
   },
 {
     timestamps: true
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mongoose =require('mongoose');
 const dotenv = require('dotenv');
 const UserRouter = require('./Routes/userRoute');
 const workerRouter = require('./Routes/workerRoute');
+const cors = require("cors");
+
 dotenv.config();
 
 const app = express();
@@ -10,6 +12,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/workers", workerRouter);
 app.use("/api/users", UserRouter);
